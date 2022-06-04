3f8b0e95a2927dd81d08413708b4dbc01a434659
Ali Mohammed <alimohammed@gmail.com>
2022-06-03 23:15:07 -0700

    determining wherther is positive ,negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
- /**
-  * main - prints a random number and state wether
-  * it is positive, negative, or negative.
-  *
-  * Return: Always 0.
-  */
-
+/**
+ * main - Prints a random number and states whether
+ * it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-{ 
+{
 	int n;
 
 	srand(time(0));
@@ -19,9 +18,8 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is positive\n", n);
+		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-
-	retun (0);
+	return (0);
 }
