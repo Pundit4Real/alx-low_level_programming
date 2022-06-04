0a02a1f7408e2ba3f483eb95fde5f2be52ecd331
Ali Mohammed <alimohammed@gmail.com>
2022-06-04 00:52:31 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,19 @@ int main(void)
 
 	int num;
 
+
 	for (num = 0; num <= 9; num++)
 
 	{
 
-		putchar((num % 10) + '0')
+		putchar((num % 10) + '0');
+
 		if (num == 9)
 			continue;
 
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
 
 	return (0);
