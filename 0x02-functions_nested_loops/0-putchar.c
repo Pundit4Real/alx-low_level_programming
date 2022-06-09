54a2ab819dc98678c3d4d9ed7fc938829357ef98
Ali Mohammed <alimohammed@gmail.com>
2022-06-09 15:12:54 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+
+/**
+ * main - Print the word "Holberton"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+		for (i = 0; i < 9; i++)
+	{
+
+		_putchar(p[i]);
+	}
+
+	_putchar('\n');
+		return (0);
+}
