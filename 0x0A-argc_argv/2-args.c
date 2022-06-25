febc403436099a606049a7e8c6dc62444a58f1c1
Ali Mohammed <alimohammed@gmail.com>
2022-06-24 21:25:32 -0700

    it print all arguments it recieve

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print arguments received
+ * @argc: length of the arguments
+ * @argv: arguments passed
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+
+	return (0);
+}
