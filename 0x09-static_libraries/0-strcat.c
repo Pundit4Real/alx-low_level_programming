6ba53f8e7e4a605bdcc4f3a94affd6e92db72556
Ali Mohammed <alimohammed@gmail.com>
2022-06-24 22:33:13 -0700

    strlen

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+* *_strcat - concatenates two strings
+* @dest: pointer destination
+* @src: pointer source
+* Return: void
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	a = 0;
-	while (dest[a] != '\0')
-	{
-		a++;
-	}
-	b = 0;
-	while (src[b] != '\0')
-	{
-		dest[a] = src[b];
-		a++;
+do {
+a++;
+dest[i] = src[a];
+i++;
+} while (src[a] != '\0');
+
+return (dest);
+}
+
