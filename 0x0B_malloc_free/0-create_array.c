29ca6fe38d15844b6648bb1725fde2424297dc3d
Ali Mohammed <alimohammed@gmail.com>
2022-06-28 15:18:37 -0700

    task 0

--- 0x0B_malloc_free/0-create_array.c
+++ 0x0B_malloc_free/0-create_array.c
@@ -10,19 +10,19 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-	unsigned int i;
+        char *str;
+        unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-	str = malloc(sizeof(char) * size);
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		str[i] = c;
-		i++;
-	}
-	return (str);
+        if (size == 0)
+                return (NULL);
+        str = malloc(sizeof(char) * size);
+        if (str == NULL)
+                return (NULL);
+        i = 0;
+        while (i < size)
+        {
+                str[i] = c;
+                i++;
+        }
+        return (str);
 }
