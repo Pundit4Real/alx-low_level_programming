76a4ebce90e1e59b02f3626457b123c13f7ec847
Ali Mohammed <alimohammed@gmail.com>
2022-07-07 19:44:34 -0700

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,40 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * main - performs operations
- * @argc: defines length of the arguments
- * @argv: defines the arguments
- * Return: 0
- */
 
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-int i;
-int j;
-int (*result)(int, int);
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
-if (argv[2][1])
-{
-printf("Error\n");
-exit(99);
-}
-result = get_op_func(argv[2]);
-if (result == NULL)
-{
-printf("Error\n");
-exit(99);
-}
-i = atoi(argv[1]);
-j = atoi(argv[3]);
-printf("%d\n", result(i, j));
-return (0);
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
 }
